c76a3704a5fe6990497890e05471a91f97586cb2
dawon020411 <dawon020411@gmail.com>
2025-06-21 00:14:00 +0900

    Deploy protfolio

---
+++ assets/index.ts
@@ -0,0 +1,37 @@
+import logo from "./images/LogoBig.png";
+import profileImgCircle from "./images/Devon_circle.png";
+import JavaScriptL from "./images/Skills/JavaScriptL.png";
+import TypeScript from "./images/Skills/TypeScript.png";
+import Python from "./images/Skills/Python.png";
+import ReactL from "./images/Skills/React.png";
+import Next from "./images/Skills/Next.png";
+import Redux from "./images/Skills/Redux.png";
+import NodeJS from "./images/Skills/NodeJS.png";
+import Docker from "./images/Skills/Docker.png";
+import Jest from "./images/Skills/Jest.png";
+import Playwright from "./images/Skills/Playwright.png";
+import Tailwind from "./images/Skills/Tailwind.png";
+import Vite from "./images/Skills/Vite.png";
+import Remix from "./images/Skills/Remix.png";
+import Neovim from "./images/Skills/Neovim.png";
+import Electron from "./images/Skills/Electron.png";
+
+export {
+  ReactL,
+  Next,
+  Redux,
+  NodeJS,
+  JavaScriptL,
+  TypeScript,
+  Python,
+  Docker,
+  Jest,
+  Playwright,
+  Tailwind,
+  Vite,
+  logo,
+  profileImgCircle,
+  Remix,
+  Neovim,
+  Electron,
+};
